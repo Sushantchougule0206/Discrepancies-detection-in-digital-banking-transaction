a16ab3bd3a8fa32ac8eb0c1783dbd2ff6d5d5a49
Sushantchougule0206 <88930989+Sushantchougule0206@users.noreply.github.com>
2023-08-23 19:34:00 +0530

    Add files via upload

---
+++ About.php
@@ -0,0 +1,35 @@
+		<div class="row">
+		    <div class="col-md-12">
+			    <div class="breadcrumbs">
+				    <ul class="breadcrumb">
+                        <li><a href="#">Home</a> <span class="divider"></span></li>
+                        <li class="active">About US</li>
+                    </ul>
+				</div>
+			</div>
+		</div>
+		
+		<div class="row">
+		    <div class="col-md-12">
+				<h2>About US</h2>
+			</div>
+		</div>
+
+		<div class="row registerbox">
+		    <div class="col-md-12">
+					
+					<h1>What</h1>
+<p class="text_about"> Info </p>
+
+<h1>Why</h1>
+<p class="text_about">Info.</p>
+
+<h1>How</h1>
+<p class="text_about">Info.</p>
+			</div>
+		</div>
+		
+
+	
+
+	
